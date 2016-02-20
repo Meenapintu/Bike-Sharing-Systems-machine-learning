f42194dccefaf97c5b70cec380776e1eaf1f88e8
Pintu Lal <ramchandratnapura@gmail.com>
2016-02-20 22:31:35 +0530

    source file added

---
+++ intro.py
@@ -0,0 +1,112 @@
+#PINTULAL 
+#120050018
+#CS403 (Foundations of Machine Learning_) Assignment 1.
+
+#-------------------------------------------------------------------------------------------
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+from sklearn import cross_validation
+from sklearn.svm import SVR
+
+
+df = pd.read_csv('bikeDataTrainingUpload.csv',header=0)
+df2 = pd.read_csv('TestX.csv',header=0)
+#print("----------------------------------------------------------------------------------")
+#df.info()
+
+cols = ['casual','registered']
+df = df.drop(cols,axis=1)
+#print("\n")
+#print("----------------------------------------------------------------------------------")
+#df.info()
+
+
+dummies = []
+cols = ['season','yr','mnth','holiday','weekday','workingday','weathersit']
+
+for col in cols:
+	dummies.append(pd.get_dummies(df[col]))
+
+titanic_dummies = pd.concat(dummies, axis=1)
+ 
+df = pd.concat((df,titanic_dummies),axis=1)
+
+
+df = df.drop(cols,axis=1)
+
+#df.info()
+
+
+#-----------------------------------------------------------------------------------
+
+dummies2 = []
+
+
+for col in cols:
+	dummies2.append(pd.get_dummies(df2[col]))
+
+titanic_dummies2 = pd.concat(dummies2, axis=1)
+ 
+df2 = pd.concat((df2,titanic_dummies2),axis=1)
+
+
+df2 = df2.drop(cols,axis=1)
+
+
+
+#df['Age'] = df['Age'].interpolate()
+
+#print("\n")
+#print("----------------------------------------------------------------------------------")
+#df.info()
+#df2.info()
+
+
+
+
+X = df.values
+y = df['cnt'].values
+
+X = np.delete(X,4,axis=1)
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size=0.0,random_state=0)
+
+#clf = linear_model.LinearRegression() #Ridge(alpha=1.0) 
+
+svr_rbf = SVR(kernel='rbf', C=1e4, gamma=0.05)
+
+
+
+#clf.fit(X_train,y_train)
+
+
+#------------------------------------------------------------------------------------------
+X2 = df2.values
+
+#X_train2, X_test2, y_train2, y_test = cross_validation.train_test_split(X2,y2,test_size=0.0,random_state=0)
+
+pred = []
+#pred=clf.predict(X2)
+
+pred = svr_rbf.fit(X_train,y_train).predict(X2)
+
+#print("\n")
+#print("----------------------------------------------------------------------------------")
+
+pred = np.abs(pred)
+raw_data = {'cnt':pred}
+df3 = pd.DataFrame(raw_data, columns = ['cnt'])
+df3.to_csv('output.csv',index_label='id')
+
+
+
+
+#print(clf.score(X_test,y_test))
+
+
+'''
+clf = tree.DecisionTreeClassifier(max_depth=5)
+scores = cross_validation.cross_val_score(clf, X, y, cv=5)
+print("----------------------------------------------------------------------------------")
+print(scores)'''
